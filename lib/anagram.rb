44d4f89f98f84ba178632cfe3e0b11238c733b3b
Daniel Calvin <arockne2003@yahoo.com>
2021-10-02 15:15:50 -0700

    commit tree 0b302323c4f7269f78c1c7e6fc7f80efc6ba3cda

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+    anagram_word = word.chars
+    possible_anagrams.filter do |w|
+      w.chars.sort == anagram_word.sort
+    end
+  end
+end
